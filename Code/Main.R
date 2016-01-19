f44edce716602af6fb1d6467299771a9eea4c4e7
Frederik <frederik.pahde@sap.com>
2016-01-19 17:12:03 +0100

    merge!

--- Code/Main.R
+++ Code/Main.R
@@ -14,7 +14,7 @@ source(paste0(dir, "/Code/PlotHelper.R"))
 
 #Script to install and load needed packages
 source(paste0(dir, "/Code/Init.R")) 
-cl <- makeCluster((detectCores()/2))
+cl <- makeCluster((detectCores()))
 registerDoParallel(cl)
 #Load Data
 source(paste0(dir, "/Code/DataLoader.R"))
@@ -38,7 +38,7 @@ trainingset <- loadImputedTrainingset(paste0(dir, "/Data/ImputedData.csv"))
 numericVariables = getNumericVariables(trainingset)
 categoricVariables <- trainingset[setdiff(colnames(trainingset), colnames(numericVariables))]
 print("Finished Missing Value Handling")
-trainingset <- trainingset[sample(1:50000,20000, replace = FALSE),]
+#trainingset <- trainingset[sample(1:50000,20000, replace = FALSE),]
 
 #Outlier Handling
 source(paste0(dir, "/Code/Outliers.R"))
@@ -144,20 +144,20 @@ errorRates.ensemble_wo <- c()
 
 #save(rf, file = "rfModel.RData")
 
-k <- 10
-res <- c()
-res_wo <- c()
+k <- 1
+#res <- c()
+#res_wo <- c()
 
 for (i in c(1:k)) {
   print(paste("start Iteration ", i))
 
   #Split to test/trainigsset
-  idx.train <- createDataPartition(y = trainingset$churn, p=(1-1/k), list=FALSE)
-  data.tr <- trainingset[idx.train,]
-  data.ts <- trainingset[-idx.train,]
+  #idx.train <- createDataPartition(y = trainingset$churn, p=(1-1/k), list=FALSE)
+  data.tr <- trainingset#[idx.train,]
+  #data.ts <- trainingset[-idx.train,]
   
-  data.tr_wo <- trainingset_withoutOutlier[idx.train,]
-  data.ts_wo <- trainingset_withoutOutlier[-idx.train,]
+  data.tr_wo <- trainingset_withoutOutlier#[idx.train,]
+  #data.ts_wo <- trainingset_withoutOutlier[-idx.train,]
   
   
   #Train Models
@@ -166,18 +166,20 @@ for (i in c(1:k)) {
   #naiveBayes <- trainNaiveBayes(data.tr)
   #print("Finished Naive Bayes Training")
   #lr <- trainLogisticRegression(data.tr_wo)
-  print("Finished Logistic Regression")
+  #print("Finished Logistic Regression")
   rf <- trainRandomForest(data.tr)
+  save(rf, file = "randomForestModel.RData")
   print("Finished Random Forest")
   #knn <- trainKNN(data.tr)
   #print("Finished KNN Training")
   #svm <- trainSVM(data.tr_wo)
-  print("Finished SVM Training")
+  #print("Finished SVM Training")
   #J48 <- trainJ48(data.tr)
   #print("Finished J48 Training")
   #adaBag <- trainAdaBag(data.tr)
   #print("Finished AdaBag Training")
   gbm <- trainGradientBoosting(data.tr)
+  save(gbm, file = "gradientBoostingModel.RData")
   print("Finished Gradient Boosting Training")
   
   #greedy_ensemble <- trainEnsembledMethod2(data.tr)
@@ -195,50 +197,50 @@ for (i in c(1:k)) {
   #yhat.nnet <- predict(nnet, newdata = data.ts_wo, type="prob")[,2]
   #yhat.naiveBayes <- predict(naiveBayes, newdata = data.ts, type="raw")
   #yhat.lr <- predict(lr, newdata = data.ts_wo, type="prob")[,2]
-  yhat.rf <- predict(rf, newdata = data.ts, type = "prob")[,2]
+  #yhat.rf <- predict(rf, newdata = data.ts, type = "prob")[,2]
   #yhat.knn <- predict(knn, newdata = data.ts, type = "raw")
   #yhat.svm <- predict(svm, newdata = data.ts_wo, type = "prob")[,2]
-  yhat.gbm <- predict(gbm, newdata = data.ts, type = "prob")[,2]
+  #yhat.gbm <- predict(gbm, newdata = data.ts, type = "prob")[,2]
   #yhat.J48 <- predict(J48, newdata = data.ts, type = "raw")
   #yhat_ensG <- predict(greedy_ensemble, newdata = data.ts)
   #yhat_ens_wo <- predict(greedy_ensemble_wo, newdata = data.ts_wo)
-  yhat_ens <- (yhat.rf + yhat.gbm)/2#predictionWrapper(models, data.ts, data.ts_wo)
+  #yhat_ens <- (yhat.rf + yhat.gbm)/2#predictionWrapper(models, data.ts, data.ts_wo)
   
   #Assess Models
   #err.nnet <- ModelPerformanceByClass(data.ts_wo$churn, yhat.nnet)
   #err.naiveBayes <-  ModelPerformanceByClass(data.ts$churn, yhat.naiveBayes)
-  lm.lr <- 0 #getLiftMeasure(data.ts_wo$churn, yhat.lr)
-  lm.rf <- getLiftMeasure(data.ts$churn, yhat.rf)
+  #lm.lr <- 0 #getLiftMeasure(data.ts_wo$churn, yhat.lr)
+  #lm.rf <- getLiftMeasure(data.ts$churn, yhat.rf)
   #err.knn <- ModelPerformanceByClass(data.ts$churn, yhat.knn)
-  lm.svm <- 0 #getLiftMeasure(data.ts$churn, yhat.svm)
-  lm.gbm <- getLiftMeasure(data.ts$churn, yhat.gbm)
+  #lm.svm <- 0 #getLiftMeasure(data.ts$churn, yhat.svm)
+  #lm.gbm <- getLiftMeasure(data.ts$churn, yhat.gbm)
   #err.J48 <- ModelPerformanceByClass(data.ts$churn, yhat.J48)
   #err.ensemble_wo <- ModelPerformance(data.ts$churn, yhat_ens_wo)
-  lm.ensemble <- getLiftMeasure(data.ts$churn, yhat_ens)
-  lm.ensembleG <- 0 #getLiftMeasure(data.ts$churn, yhat_ensG)
+  #lm.ensemble <- getLiftMeasure(data.ts$churn, yhat_ens)
+  #lm.ensembleG <- 0 #getLiftMeasure(data.ts$churn, yhat_ensG)
   #err.rf <- getLiftMeasure(data.ts$churn, yhat.rf)
   
   #print(paste0("ErrorRate (nnet): ", err.nnet))
   #print(paste0("ErrorRate (naiveBayes): ", err.naiveBayes))
-  print(paste0("LiftMeasure (Logistic Regression): ", lm.lr))
-  print(paste0("LiftMeasure (Random Forest): ", lm.rf))
+  #print(paste0("LiftMeasure (Logistic Regression): ", lm.lr))
+  #print(paste0("LiftMeasure (Random Forest): ", lm.rf))
   #print(paste0("ErrorRate (KNN): ", err.knn))
-  print(paste0("LiftMeasure (svm): ", lm.svm))
-  print(paste0("LiftMeasure (GBM): ", lm.gbm))
+  #print(paste0("LiftMeasure (svm): ", lm.svm))
+  #print(paste0("LiftMeasure (GBM): ", lm.gbm))
   #print(paste0("ErrorRate (J48): ", err.J48))
-  print(paste0("LiftMeasure (Ensemble): ", lm.ensemble))
-  print(paste0("LiftMeasure (EnsembleCaret): ", lm.ensembleG))
+  #print(paste0("LiftMeasure (Ensemble): ", lm.ensemble))
+  #print(paste0("LiftMeasure (EnsembleCaret): ", lm.ensembleG))
   #print(paste0("LiftMeasure (Ensemble WO): ", lm.ensemble_wo))
   
   #errorRates.nnet <- c(errorRates.nnet, err.nnet)
   #errorRates.naiveBayes <- c(errorRates.naiveBayes, err.naiveBayes)
-  lms.lr <- c(lms.lr, lm.lr)
-  lms.rf <- c(lms.rf, lm.rf)
+  #lms.lr <- c(lms.lr, lm.lr)
+  #lms.rf <- c(lms.rf, lm.rf)
   #errorRates.knn <- c(errorRates.knn, err.knn)
-  lms.svm <- c(lms.svm, lm.svm)
-  lms.gbm <- c(lms.gbm, lm.gbm)
-  lms.ensemble <- c(lms.ensemble, lm.ensemble)
-  lms.ensembleG <- c(lms.ensemble, lm.ensembleG)
+  #lms.svm <- c(lms.svm, lm.svm)
+  #lms.gbm <- c(lms.gbm, lm.gbm)
+  #lms.ensemble <- c(lms.ensemble, lm.ensemble)
+  #lms.ensembleG <- c(lms.ensemble, lm.ensembleG)
   #errorRates.J48 <- c(errorRates.J48, err.J48)
   #res <- c(res, lm.ensemble)
   #res_wo <- c(res_wo, lm.ensemble_wo)
@@ -249,12 +251,12 @@ for (i in c(1:k)) {
   #                                                                       Lift Measure: ", lm.ensemble, 
   #                                                                       "\nLift Measure (wo): ", lm.ensemble_wo))
   
-  sendmail("frederik@pahde.com", subject="R Notification", message=paste("Finished Iteration ", i, ": 
-                                                                        Logistic Regression: ", lm.lr, "\n
-                                                                        Random Forest: ", lm.rf, "\n
-                                                                        Gradient Boosting: ", lm.gbm, "\n
-                                                                        Ensemble (caret): ", lm.ensembleG, "\n
-                                                                        Ensemble (Own): ", lm.ensemble), password="rmail")
+  #sendmail("frederik@pahde.com", subject="R Notification", message=paste("Finished Iteration ", i, ": 
+  #                                                                      Logistic Regression: ", lm.lr, "\n
+  #                                                                      Random Forest: ", lm.rf, "\n
+  #                                                                      Gradient Boosting: ", lm.gbm, "\n
+  #                                                                      Ensemble (caret): ", lm.ensembleG, "\n
+  #                                                                      Ensemble (Own): ", lm.ensemble), password="rmail")
 
   
 }
@@ -263,20 +265,20 @@ for (i in c(1:k)) {
 #print(errorRates.nnet)
 #print("Error Rates Naive Bayes: ")
 #print(errorRates.naiveBayes)
-print("Lift Measure LR: ")
-print(lms.lr)
-print("Lift Measure RF: ")
-print(lms.rf)
+#print("Lift Measure LR: ")
+#print(lms.lr)
+#print("Lift Measure RF: ")
+#print(lms.rf)
 #print("Error Rates KNN: ")
 #print(errorRates.knn)
-print("Lift Measure SVM: ")
-print(lms.svm)
-print("Lift Measure GBM: ")
-print(lms.gbm)
-print("Lift Measure Ensemble (Caret): ")
-print(lms.ensembleG)
-print("Lift Measure Ensemble (Own): ")
-print(lms.ensemble)
+#print("Lift Measure SVM: ")
+#print(lms.svm)
+#print("Lift Measure GBM: ")
+#print(lms.gbm)
+#print("Lift Measure Ensemble (Caret): ")
+#print(lms.ensembleG)
+#print("Lift Measure Ensemble (Own): ")
+#print(lms.ensemble)
 #print("Error Rates J48: ")
 #print(errorRates.J48)
 #print("Error Rates Ensemble: ")
